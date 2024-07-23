91fcee3aa8164121250363c782d8e90308e2d799
Luan Truong 2802 <truongluan280293@gmail.com>
2024-07-23 21:12:58 +0700

    Initial Project

---
+++ generalcomponents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  generalcomponents
+//
+//  Created by Truong Luan on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ generalcomponents/generalcomponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  generalcomponentsApp.swift
+//  generalcomponents
+//
+//  Created by Truong Luan on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct generalcomponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
