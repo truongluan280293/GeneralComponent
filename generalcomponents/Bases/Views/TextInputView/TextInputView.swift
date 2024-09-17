b2e62958e42d0cfc2d7bac9252f82ddaee0fb094
Luan Truong 2802 <truongluan280293@gmail.com>
2024-09-17 20:57:25 +0700

    Setup custom textfield

---
+++ generalcomponents/Bases/Views/TextInputView/TextInputView.swift
@@ -0,0 +1,18 @@
+//
+//  TextInputView.swift
+//  generalcomponents
+//
+//  Created by Truong Luan on 9/16/24.
+//
+
+import SwiftUI
+
+struct TextInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextInputView()
+}
